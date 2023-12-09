0e55fd731e1c912e272f9497ddf64c3135cfa623
TuanDinh140194 <tnd@pdx.edu>
2023-12-09 00:54:01 -0800

    fix fb

--- src/components/FacebookMsg.tsx
+++ src/components/FacebookMsg.tsx
@@ -5,7 +5,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 function FacebookMsg() {
   return (
     <FacebookProvider appId="761457112471246" chatSupport>
-      <CustomChat pageId="137961782734683" minimized={`true`}/>
+      <CustomChat pageId="137961782734683" minimized={true}/>
     </FacebookProvider>
   );
 }
