63d25a1f3c9a668fdd4226f005f59b71564e8868
TuanDinh140194 <tnd@pdx.edu>
2023-12-21 23:34:06 -0800

    fix payment

--- src/utils/get-stripejs.ts
+++ src/utils/get-stripejs.ts
@@ -6,7 +6,7 @@ const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(
       process.env.STRIPE_PUBLIC_KEY ??
-        "pk_test_51N71cdDTggahj9sxb0nEAKsxnQK9W66BXay41Y8GUlh9ODtPP4TEqWMBxoWAPN423b4lrvmgO0FvCbvqNJj42udW00DcJfyCWZ",
+        "pk_live_51ON9NiIyNJBxTIRY7I2UZc8W1XEV87cK0e2tmA6hm9X0d9ekL1BjANGsEbZql0Mfmpa4umqGURJhPA14Zt9XYRmT00HaQA4u20",
     );
   }
   return stripePromise;
